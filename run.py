3ad5905fb85e698c016985e9716c147505861c20
manymore13 <manymore13@qq.com>
2024-01-31 00:31:15 +0800

    修改部署环境

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from scrapy import cmdline
 
 from report.utils import get_start_end_date
 
-start_date, end_date = get_start_end_date(2)
+start_date, end_date = get_start_end_date(1)
 
 cmdline.execute('scrapy crawl east_report -a codes=* -a count=101 -a begin_time={} -a end_time={}'.format(start_date,
                                                                                                           end_date).split())
