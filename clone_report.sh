be77bed7a7de0d0bac1db625e470a64ca4c0c309
e-Zhiwei.Yan <e-Zhiwei.Yan@lotuscars.com.cn>
2024-01-31 12:34:47 +0800

    脚本修改

---
+++ clone_report.sh
@@ -0,0 +1,13 @@
+if [ -d "target" ]; then
+    # 目录存在，删除它
+    rm -r "target"
+    echo "Directory deleted."
+else
+    echo "Directory does not exist."
+fi
+mkdir target
+cd target
+git clone https://github.com/manymore13/report.git
+cd ..
+mkdir gen_east_report
+cp -r target/report/* gen_east_report
