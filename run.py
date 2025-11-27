cccf218ef38f6b6e42e5573c878eb3cf14328390
manymore13 <manymore13@qq.com>
2025-11-27 12:32:24 +0800

    Remove duplicate subprocess call for index_data.py

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from report.utils import get_start_end_date
 
 start_date, end_date = get_start_end_date(180)
 
+subprocess.run('python index_data.py', shell=True)
 cmdline.execute('scrapy crawl east_report -a codes=* -a count=101 -a begin_time={} -a end_time={}'.format(start_date,
                                                                                                           end_date).split())
-subprocess.run('python index_data.py', shell=True)
+
 
