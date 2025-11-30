be371bd0849b893d68792b0925f19a8572166051
manymore13 <manymore13@qq.com>
2025-11-30 23:13:49 +0800

    Add subprocess call for bond_data.py

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from report.utils import get_start_end_date
 start_date, end_date = get_start_end_date(180)
 
 subprocess.run('python index_data.py', shell=True)
+subprocess.run('python bond_data.py', shell=True)
 cmdline.execute('scrapy crawl east_report -a codes=* -a count=101 -a begin_time={} -a end_time={}'.format(start_date,
                                                                                                           end_date).split())
 
