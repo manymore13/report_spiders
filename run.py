cf73b87962767ff97c72d5ebb6496127befcfce0
manymore13 <manymore13@qq.com>
2025-11-30 23:45:08 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from scrapy import cmdline
-import subprocess
+import os
 
 from report.utils import get_start_end_date
 
 start_date, end_date = get_start_end_date(180)
 
-subprocess.run('python index_data.py', shell=True)
-subprocess.run('python bond_data.py', shell=True)
+scripts = ['index_data.py', 'bond_data.py']
+for script in scripts:
+    os.system(f'python {script}')
+
 cmdline.execute('scrapy crawl east_report -a codes=* -a count=101 -a begin_time={} -a end_time={}'.format(start_date,
                                                                                                           end_date).split())
 
